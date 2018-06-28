a9676d4bc7dcf3937e7488ffc8554d83c0c6c263
Steven Isbell <steveni_92@msn.com>
2018-06-28 17:20:18 -0500

    updates

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const { json } = require('body-parser');
-const cors = require('cors');
 
 const port = 3001;
 
 const app = express();
 
 app.use(json());
-app.use(cors());
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
