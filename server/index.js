f30c1fe34530b38fc0d5613dc815e042227a38fc
Steven Isbell <steveni_92@msn.com>
2018-08-01 00:03:46 -0500

    updated solution

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
+const { readFileSync } = require('fs');
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = require('./schema/typeDefs');
+const typeDefs = readFileSync(`${__dirname}/schema/typeDefs.graphql`, 'utf8');
 const resolvers = require('./schema/resolvers');
+
+const options = {
+  port: 3001,
+  endpoint: '/graphql',
+  playground: '/graphiql'
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: req => ({
+    ...req.request
+  })
+});
 
 server.start(options, () =>
   console.log(`Server is running on localhost:${options.port}`)
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,1 +1,32 @@
 const products = require('../model');
+
+// Can't stick cart on session because graphql won't persist
+// the session when using graphiql
+let cart = [];
+
+const resolvers = {
+  Query: {
+    products() {
+      return products;
+    },
+    product(_, { id }) {
+      return products.find(val => val.id === +id);
+    },
+    cart(_, args, req) {
+      if (cart.length) return cart;
+      else throw new Error('Cart is empty!');
+    }
+  },
+  Mutation: {
+    addToCart(_, { id }, req) {
+      const item = products.find(val => val.id === +id);
+      cart.push(item);
+      return cart;
+    },
+    deleteFromCart(_, { id }, req) {
+      cart = cart.filter(val => val.id !== +id);
+    }
+  }
+};
+
+module.exports = resolvers;
