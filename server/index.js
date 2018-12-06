2a5192ce9e0830726006d1b9d8666f81a9c92e17
Steven Isbell <steveni_92@msn.com>
2018-12-06 08:53:12 -0700

    cleaning master

--- server/index.js
+++ server/index.js
@@ -1,5 +1,1 @@
-const { GraphQLServer } = require('graphql-yoga');
-const { readFileSync } = require('fs');
 
-const typeDefs = readFileSync(`${__dirname}/schema/typeDefs.graphql`, 'utf8');
-const resolvers = require('./schema/resolvers');
